66edd5b29ad9ddc2db238041b79c3afe4b648a33
Solitary <2213096142@qq.com>
2022-10-17 20:30:06 +0800

    init

---
+++ src/store/modules/user.js
@@ -0,0 +1,31 @@
+import { login, logout, getInfo } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { resetRouter } from '@/router'
+
+const getDefaultState = () => {
+  return {
+    token: getToken(),
+    name: '',
+    avatar: ''
+  }
+}
+
+const state = getDefaultState()
+
+const mutations = {
+  RESET_STATE: (state) => {
+    Object.assign(state, getDefaultState())
+  },
+}
+
+const actions = {
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
